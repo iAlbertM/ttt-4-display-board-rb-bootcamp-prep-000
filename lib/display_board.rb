d4699f9cb5e622cae375df4009c5d22ba17c9fe6
Albert Martinez <albertwdd@gmail.com>
2019-09-04 17:57:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,10 @@
 def display_board(board)
   divider = "-----------"
   
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts divider
-  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts divider
-  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
   
